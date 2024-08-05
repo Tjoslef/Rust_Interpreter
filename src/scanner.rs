6ff4efa8903be2873cbe29dfb13cc0bf23d74ed0
Tjoslef <josef.pasek17@gmail.com>
2024-08-05 14:49:38 +0200

    codecrafters submit [skip ci]

--- src/scanner.rs
+++ src/scanner.rs
@@ -13,6 +13,18 @@ pub fn print_lexemen(content: String){
 
             '{' => println!("LEFT_BRACE {{ null"),
 
+            '*' => println!("STAR * null"),
+
+            '.' => println!("DOT . null"),
+
+            ',' => println!("COMMA , null"),
+
+            '+' => println!("PLUS + null"),
+
+            '-' => println!("MINUS - null"),
+
+            ';' => println!("SEMICOLON ; null"),
+
             _ => println!("unknown {}", char),
         }
     }

a71ce67931aae3d74656c7ef45a68a85383911f0
Tjoslef <josef.pasek17@gmail.com>
2024-08-07 09:17:13 +0200

    update debuging

---
+++ src/token.rs
@@ -0,0 +1,61 @@
+use std::fmt::Display;
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub enum TokenType {
+
+    LEFT_PAREN,
+
+    RIGHT_PAREN,
+
+    LEFT_BRACE,
+
+    RIGHT_BRACE,
+
+    COMMA,
+
+    DOT,
+
+    MINUS,
+
+    PLUS,
+
+    SEMICOLON,
+
+    STAR,
+
+    FOWARD_SLASH,
+
+    EQUAL,
+
+    EQUAL_EQUAL,
+
+    EOF,
+
+}
+pub struct Token {
+    _type: TokenType,
+    _string: String,
+    _value: Option<String>,
+
+}
+impl Token {
+    pub fn new(_type: TokenType, _string: String) -> Self {
+        Token {
+            _type,
+            _string,
+            _value: None,
+        }
+    }
+}
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:?} {} {}",
+            self._type,
+            self._string,
+            self._value.clone().unwrap_or("null".to_string())
+        )
+    }
+}
+
---
+++ src/tokenizer.rs
@@ -0,0 +1,53 @@
+use anyhow::{bail};
+use std::fs;
+use crate::token::{Token, TokenType};
+use crate::error::{Error};
+
+pub fn tokenize(filename: &String) -> anyhow::Result<()> {
+    let file_contents = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(_) => bail!("Failed to read the file."),
+    };
+    let mut char_cont = file_contents.chars();
+    let line = 1usize;
+    let mut has_error = false;
+    let mut token = vec![];
+    while let Some(c) = char_cont.next() {
+        match c {
+            '(' => token.push(Token::new(TokenType::LEFT_PAREN, c.to_string())),
+            ')' => token.push(Token::new(TokenType::RIGHT_PAREN, c.to_string())),
+            '{' => token.push(Token::new(TokenType::LEFT_BRACE, c.to_string())),
+            '}' => token.push(Token::new(TokenType::RIGHT_BRACE, c.to_string())),
+            ',' => token.push(Token::new(TokenType::COMMA, c.to_string())),
+            '.' => token.push(Token::new(TokenType::DOT, c.to_string())),
+            '-' => token.push(Token::new(TokenType::MINUS, c.to_string())),
+            '+' => token.push(Token::new(TokenType::PLUS, c.to_string())),
+            ';' => token.push(Token::new(TokenType::SEMICOLON, c.to_string())),
+            '*' => token.push(Token::new(TokenType::STAR, c.to_string())),
+            '/' => token.push(Token::new(TokenType::FOWARD_SLASH, c.to_string())),
+            '=' => {
+                let mut peekable = char_cont.clone().peekable();
+                if peekable.peek() == Some(&'=') {
+                    token.push(Token::new(TokenType::EQUAL_EQUAL, "==".to_string()));
+                    char_cont.next();
+                } else {
+                    token.push(Token::new(TokenType::EQUAL, c.to_string()));
+                }
+            },
+            _ => {
+                eprintln!("[line {}] Error: Unexpected character: {}", line, c);
+                has_error = true
+            }
+
+        }
+    }
+    token.push(Token::new(TokenType::EOF, "".to_string()));
+    for token in &token {
+        println!("{}", token);
+    }
+    return if has_error {
+        bail!(Error::new(65))
+    } else {
+        Ok(())
+    };
+}

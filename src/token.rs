414b7e3ad5cffaeb91fd3876e20cacc77a9c6298
Tjoslef <josef.pasek17@gmail.com>
2024-08-11 00:34:30 +0200

    codecrafters submit [skip ci]

--- src/token.rs
+++ src/token.rs
@@ -1,78 +1,66 @@
-use std::fmt;
+
 use std::fmt::Display;
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
 pub enum TokenType {
-
     LEFT_PAREN,
-
     RIGHT_PAREN,
-
     LEFT_BRACE,
-
     RIGHT_BRACE,
-
     COMMA,
-
     DOT,
-
     MINUS,
-
     PLUS,
-
     SEMICOLON,
-
     STAR,
-
     SLASH,
-
     EQUAL,
-
     EQUAL_EQUAL,
-
     BANG_EQUAL,
-
     BANG,
-
     GREATER_EQUAL,
-
     GREATER,
-
     LESS_EQUAL,
-
     LESS,
-
     STRING,
-
     EOF,
+    NUMBER,
+    ROUNDED_NUMBER,
 }
 pub struct Token {
     _type: TokenType,
     _string: String,
-    _value: Option<String>,
+    _value: String,
 
 }
 impl Token {
-    pub fn new(_type: TokenType, _string: String) -> Self {
+    pub fn new(_type: TokenType, _string: String, _value:String ) -> Self {
         Token {
             _type,
             _string,
-            _value: None,
+            _value,
         }
     }
 }
 impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self._type {
-            TokenType::STRING => write!(f,"{:?} \"{}\" {}",self._type,self._value.clone().unwrap_or(self._string.to_string()),self._string),
-
+            TokenType::STRING => write!(f,"{:?} \"{}\" {}",self._type,self._string,self._string),
+            TokenType::NUMBER =>{ if self._string.ends_with(".0") {
+                write!(f, "NUMBER {} {}", self._string.replace(".0", ""), self._string)
+            } else if !self._string.contains(".") {
+                write!(f, "NUMBER {} {}.0", self._string, self._string)
+            } else {
+                write!(f, "NUMBER {} {}", self._string, self._string)
+            } }
+           // TokenType::ROUNDED_NUMBER =>  write!(f,"{:?} {} {}.0","NUMBER",self._value.clone().unwrap_or(self._string.to_string()),self._string),
             _ => {
                 write!(
                     f,
-                    "{:?} {} {}",
+                    "{:?} {} {}null",
                     self._type,
                     self._string,
-                    self._value.clone().unwrap_or("null".to_string())
+                    self._value,
                 )
             }
         }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -10,29 +10,31 @@ pub fn tokenize(filename: &String) -> Result<()> {
     };
     let mut char_cont = file_contents.chars().peekable();
     let mut line = 1;
-    let mut literals = String::new();
+    let mut done = false;
+    let mut literalStr = String::new();
+    let mut literalNum = String::new();
     let mut has_error = false;
     let mut token = vec![];
-    while let Some(c) = char_cont.next() {
+  'outer:while let Some(c) = char_cont.next() {
 
         match c {
-
-            '(' => token.push(Token::new(TokenType::LEFT_PAREN, c.to_string())),
+            '(' => token.push(Token::new(TokenType::LEFT_PAREN, c.to_string(), "".to_string())),
 
             ')' =>
-                token.push(Token::new(TokenType::RIGHT_PAREN, c.to_string())),
+                token.push(Token::new(TokenType::RIGHT_PAREN, c.to_string(), "".to_string())),
 
-            '{' => token.push(Token::new(TokenType::LEFT_BRACE, c.to_string())),
+            '{' => token.push(Token::new(TokenType::LEFT_BRACE, c.to_string(), "".to_string())),
 
-            '}' => token.push(Token::new(TokenType::RIGHT_BRACE, c.to_string())),
-            ',' => token.push(Token::new(TokenType::COMMA, c.to_string())),
-            '.' => token.push(Token::new(TokenType::DOT, c.to_string())),
-            '-' => token.push(Token::new(TokenType::MINUS, c.to_string())),
-            '+' => token.push(Token::new(TokenType::PLUS, c.to_string())),
-            ';' => token.push(Token::new(TokenType::SEMICOLON, c.to_string())),
-            '*' => token.push(Token::new(TokenType::STAR, c.to_string())),
+            '}' => token.push(Token::new(TokenType::RIGHT_BRACE, c.to_string(), "".to_string())),
+            ',' => token.push(Token::new(TokenType::COMMA, c.to_string(), "".to_string())),
+            '.' => token.push(Token::new(TokenType::DOT, c.to_string(), "".to_string())),
+            '-' => token.push(Token::new(TokenType::MINUS, c.to_string(), "".to_string())),
+            '+' => {
+                token.push(Token::new(TokenType::PLUS, c.to_string(), "".to_string()));
+            }
+            ';' => token.push(Token::new(TokenType::SEMICOLON, c.to_string(), "".to_string())),
+            '*' => token.push(Token::new(TokenType::STAR, c.to_string(), "".to_string())),
             '/' => {
-
                 if let Some(&next_char) = char_cont.peek() {
                     if next_char == '/' {
                         while let Some(comment_char) = char_cont.next() {
@@ -40,69 +42,89 @@ pub fn tokenize(filename: &String) -> Result<()> {
                                 line += 1;
                                 break;
                             }
-
                         } // Continue to the next character
                     } else {
-                        token.push(Token::new(TokenType::SLASH, "/".to_string()));
+                        token.push(Token::new(TokenType::SLASH, "/".to_string(), "".to_string()));
                     }
                 } else {
-                    token.push(Token::new(TokenType::SLASH, "/".to_string()));
+                    token.push(Token::new(TokenType::SLASH, "/".to_string(), "".to_string()));
                 }
             },
             '=' => {
                 if char_cont.peek() == Some(&'=') {
-                    token.push(Token::new(TokenType::EQUAL_EQUAL, "==".to_string()));
+                    token.push(Token::new(TokenType::EQUAL_EQUAL, "==".to_string(), "".to_string()));
                     char_cont.next();
                 } else {
-                    token.push(Token::new(TokenType::EQUAL, c.to_string()));
+                    token.push(Token::new(TokenType::EQUAL, c.to_string(), "".to_string()));
                 }
             },
             '!' => {
-                if char_cont.peek() == Some(&'='){
-                    token.push(Token::new(TokenType::BANG_EQUAL,"!=".to_string()));
+                if char_cont.peek() == Some(&'=') {
+                    token.push(Token::new(TokenType::BANG_EQUAL, "!=".to_string(), "".to_string()));
                     char_cont.next();
-                }
-                else {
-                    token.push(Token::new(TokenType::BANG,c.to_string()));
+                } else {
+                    token.push(Token::new(TokenType::BANG, c.to_string(), "".to_string()));
                 }
             },
-            '>' =>{
-                if char_cont.peek() == Some(&'='){
-                    token.push(Token::new(TokenType::GREATER_EQUAL,">=".to_string()));
+            '>' => {
+                if char_cont.peek() == Some(&'=') {
+                    token.push(Token::new(TokenType::GREATER_EQUAL, ">=".to_string(), "".to_string()));
                     char_cont.next();
-                }
-                else {
-                    token.push(Token::new(TokenType::GREATER,c.to_string()));
+                } else {
+                    token.push(Token::new(TokenType::GREATER, c.to_string(), "".to_string()));
                 }
             },
             '<' => {
-                if char_cont.peek() == Some(&'='){
-                    token.push(Token::new(TokenType::LESS_EQUAL,"<=".to_string()));
+                if char_cont.peek() == Some(&'=') {
+                    token.push(Token::new(TokenType::LESS_EQUAL, "<=".to_string(), "".to_string()));
                     char_cont.next();
+                } else {
+                    token.push(Token::new(TokenType::LESS, c.to_string(), "".to_string()));
                 }
-                else {
-                    token.push(Token::new(TokenType::LESS,c.to_string()));
-                }
-                }
-            '\n' =>{
-                line +=1;
-
+            }
+            '\n' => {
+                line += 1;
             },
-            '"' =>{
+            '"' => {
                 while let Some(stringL) = char_cont.next() {
-                    if stringL =='"' {
-                        token.push(Token::new(TokenType::STRING,literals.to_string()));
-                        literals.clear();
+                    if stringL == '"' {
+                        token.push(Token::new(TokenType::STRING, literalStr.to_string(), literalNum.to_string()));
+                        literalStr.clear();
                         break;
-                    }else {
-                        literals.push(stringL);
+                    } else {
+                        literalStr.push(stringL);
                     }
                 }
-                if !literals.is_empty(){
-                    eprintln!("[line {}] Error: Unterminated string.",line);
+                if !literalStr.is_empty() {
+                    eprintln!("[line {}] Error: Unterminated string.", line);
                     has_error = true;
-                    literals.clear();
+                    literalStr.clear();
                     break;
+                }
+            },
+            c if c.is_digit(10) => {
+                let mut cont = String::from(c);
+                let mut comma_detected = false;
+                while let Some(t) = char_cont.peek() {
+                    if t.is_digit(10) {
+                        cont.push(*t);
+                        char_cont.next();
+                    } else if *t == '.' && !comma_detected {
+                        comma_detected = true;
+                        cont.push(*t);
+                        char_cont.next();
+                        }
+                    else {
+                        break;
+                    }
+
+                }
+                if cont.ends_with('.') {
+                    cont.push('0');
+                    token.push(Token::new(TokenType::NUMBER, cont.to_string(), cont.to_string()));
+                    token.push(Token::new(TokenType::DOT,".".to_string(),"".to_string()))
+                }else {
+                    token.push(Token::new(TokenType::NUMBER, cont.to_string(), cont.to_string()));
                 }
             },
             ' '|'\t'| '\r' =>{continue;},
@@ -116,13 +138,41 @@ pub fn tokenize(filename: &String) -> Result<()> {
 
         }
     }
-    token.push(Token::new(TokenType::EOF, "".to_string()));
+
+    token.push(Token::new(TokenType::EOF, "".to_string(),"".to_string()));
+
     for token in &token {
         println!("{}", token);
     }
+
     return if has_error {
         bail!(Error::new(65))
     } else {
         Ok(())
     };
+}
+pub fn rounding(literalNum: &String) -> String{
+    let mut returnNum = String::new();
+    if literalNum.is_empty() {
+        return String::new();
+    } else {
+        if let Some(dot_pos) = literalNum.find('.') {
+            let integer_part = &literalNum[..dot_pos];
+            let decimal_part = &literalNum[(dot_pos + 1)..];
+            if let Some(last_digit_char) = decimal_part.chars().nth(1) {
+                if let Some(last_digit) = last_digit_char.to_digit(10) {
+                    if last_digit < 5 {
+                        returnNum = integer_part.to_string();
+                    } else {
+                        returnNum = (integer_part.parse::<i32>().unwrap() + 1).to_string();
+                    };
+                    return returnNum;
+                }
+                return String::new();
+            }
+            return String::new();
+        }
+        return String::new();
+    }
+
 }

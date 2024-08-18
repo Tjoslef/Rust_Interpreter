b80de908bea8a2a058bf279b91340ae43404810b
Tjoslef <josef.pasek17@gmail.com>
2024-08-18 22:19:35 +0200

    codecrafters submit [skip ci]

--- src/parse.rs
+++ src/parse.rs
@@ -121,7 +121,7 @@ impl Display for Expr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Expr::BoolLite(b) => write!(f, "{b}"),
-            Expr::Literal(n) => write!(f, "{n}"),
+            Expr::FloatLit(n) => write!(f, "{n}"),
             Expr::Literal(s) => write!(f, "{s}"),
             Expr::Group(g) => {
                 write!(f, "(group {g})")
